8c45a2af6c05afde9a138668907670b9e5117745
parag khodke <49511911+paragkhodke72@users.noreply.github.com>
2020-04-26 02:34:25 -0400

    Update settings.py

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-&0x+&%&&h7gjy%$7-ip!j21#(58gf+(g9nkb-6@@w01$d9$uy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','simplesocialparag.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -119,11 +119,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
+
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-LOGIN_REDIRECT_URL = 'test'
-LOGOUT_REDIRECT_URL = 'thanks'
